21aaf23149e50ef7c518957e99cc408bdbebccd7
tsamandardev89 <tsamandardev89@gmail.com>
2025-01-29 16:59:28 +0500

    feature:binarySearch-algarithm

---
+++ algorithm/binarySearch.ts
@@ -0,0 +1,15 @@
+function binarySearch<T extends number>(arr: T[], target: T): number {
+  let left = 0,
+    right = arr.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) return mid;
+    if (arr[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+
+  return -1;
+}
+
+export default binarySearch;

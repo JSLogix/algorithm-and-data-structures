e4bc0f1e6c23f3c50b6efcbad227f0ea447f7f5a
tsamandardev89 <tsamandardev89@gmail.com>
2025-01-29 21:29:30 +0500

    feature:stack data structure in ts

--- data-structure/typescript/stack.ts
+++ data-structure/typescript/stack.ts
@@ -2,6 +2,26 @@ class Stack<T> {
   private root: T[];
 
   constructor(_val: T[] = []) {
-    this.root = _val;
+    this.root = _val.slice();
+  }
+
+  push(val: T): void {
+    this.root[this.root.length] = val;
+  }
+
+  pop(): T | undefined {
+    return this.root.length ? this.root.pop() : undefined;
+  }
+
+  peek(): T | undefined {
+    return this.root[this.root.length - 1];
+  }
+
+  size(): number {
+    return this.root.length;
+  }
+
+  isEmpty(): boolean {
+    return !this.root.length;
   }
 }
